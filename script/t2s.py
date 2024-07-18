79ad14b98e17e29cdf6686978b5efebbd2844edd
DashBing <103482780+DashBing@users.noreply.github.com>
2024-07-18 16:50:00 +0800

    add encoding

--- script/t2s.py
+++ script/t2s.py
@@ -2,15 +2,17 @@ from sys import argv
 import os
 import opencc
 
+encode_type = "utf-8"
+
 converter = opencc.OpenCC('t2s')
 t2s = lambda text: converter.convert(text)  # 文档汉字简化函数
 
 def change_file(filename):  # 简化一个文件中的汉字
     s = ""
-    with open(filename, "r") as f:
+    with open(filename, "r", encoding=encode_type) as f:
         s = f.read()
     s = t2s(s)
-    with open(filename, "w") as f:
+    with open(filename, "w", encoding=encode_type) as f:
         f.write(s)
 
 def dfs(path, do=change_file):  # 简单地深度优先搜索目录下所有文件
